5d9dda95b66a3f8a604453fe6e0507f3f08cb55c
Shinchunghwan <people_22@naver.com>
2022-11-30 16:18:41 +0900

    첨부파일이 있을때 상세 페이지에 다운로드 버튼 출력

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
+import os.path
+import os
+
+
 from django.db import models
 
 # Create your models here.
@@ -7,6 +11,8 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -19,3 +25,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split(' . ')[-1]
