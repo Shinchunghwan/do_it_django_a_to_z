5c8b615cce607513aa751b0856823f65afef38ad
Shinchunghwan <people_22@naver.com>
2022-11-30 10:30:32 +0900

    CBV 로 상세페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
+
+
+
+class PostList(ListView):
+    model = Post
+    #template_name = 'blog/index.html'
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -13,22 +24,17 @@ from .models import Post
 #         }
 #     )
 
-class PostList(ListView):
-    model = Post
-    #template_name = 'blog/index.html'
-    ordering = '-pk'
 
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
 
 
 
